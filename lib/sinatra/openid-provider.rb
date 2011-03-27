176dd8a245697807afc7e72586091c446576ec3e
Mael Clerambault <mael@clerambault.fr>
2011-03-27 01:42:55 +0100

    Sinatra helpers

---
+++ lib/sinatra/openid-provider.rb
@@ -0,0 +1,28 @@
+require 'sinatra/base'
+require 'rack/openid-provider'
+
+module Sinatra
+  module OpenIDProvider
+    
+    module Helpers
+      def openid; Rack::OpenIDRequest.new(env) end
+    end
+   
+    def checkid_setup(&block)
+      get  "/", :openid_mode => "checkid_setup", &block
+      post "/", :openid_mode => "checkid_setup", &block
+    end
+       
+    def checkid_immediate(&block)
+      get  "/", :openid_mode => "checkid_immediate", &block
+      post "/", :openid_mode => "checkid_immediate", &block
+    end
+    
+    def self.registered(app)
+      app.helpers OpenIDProvider::Helpers
+      app.set(:openid_mode) { |value| condition { openid.mode == value } }
+    end
+  end
+  
+  register OpenIDProvider
+end
